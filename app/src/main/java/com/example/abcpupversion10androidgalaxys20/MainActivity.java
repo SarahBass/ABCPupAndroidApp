c7fc7b2b0f9028efcb12115b6c43c5122f12e739
Sarah Bass <darahbass@gmail.com>
2020-08-16 22:00:40 -0700

    hackbutton

--- app/src/main/java/com/example/abcpupversion10androidgalaxys20/MainActivity.java
+++ app/src/main/java/com/example/abcpupversion10androidgalaxys20/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.abcpupversion10androidgalaxys20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.abcspacebackgrounds);
+
+        final ImageView aLetter = new ImageView(this.getApplicationContext());
+        aLetter.setImageDrawable(getDrawable(R.drawable.ic_button_for_dictionary));
+        View lettersBox = (View) findViewById(R.id.lettersBox);
+        ((LinearLayout) lettersBox).addView(aLetter);
+
+        lettersBox.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View var1, MotionEvent var2) {
+                aLetter.setX(var2.getX() - 150);
+                aLetter.setY(var2.getY() - 150);
+                return true;
+            }
+        });
     }
 }
