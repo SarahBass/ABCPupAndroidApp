3843d3dcbfaf9a5c13c97cfe7e976b5134b541bd
Sarah Bass <darahbass@gmail.com>
2020-08-16 22:39:25 -0700

    Vibrate, resize, and not hack for moving

--- app/src/main/java/com/example/abcpupversion10androidgalaxys20/MainActivity.java
+++ app/src/main/java/com/example/abcpupversion10androidgalaxys20/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.abcpupversion10androidgalaxys20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    final float scale = 0.95f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +25,29 @@ public class MainActivity extends AppCompatActivity {
         View lettersBox = (View) findViewById(R.id.lettersBox);
         ((LinearLayout) lettersBox).addView(aLetter);
 
+        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         lettersBox.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View var1, MotionEvent var2) {
-                aLetter.setX(var2.getX() - 150);
-                aLetter.setY(var2.getY() - 150);
+            public boolean onTouch(View view, MotionEvent event) {
+                aLetter.setX(event.getX() - aLetter.getWidth() / 2);
+                aLetter.setY(event.getY() - aLetter.getHeight() / 2);
+
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+
+                    aLetter.setScaleX(scale);
+                    aLetter.setScaleY(scale);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        v.vibrate(VibrationEffect.createOneShot(10, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        //deprecated in API 26
+                        v.vibrate(10);
+                    }
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    aLetter.setScaleX(1.0f);
+                    aLetter.setScaleY(1.0f);
+                }
+
                 return true;
             }
         });
